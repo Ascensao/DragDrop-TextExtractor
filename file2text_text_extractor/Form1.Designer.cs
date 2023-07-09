dcc8020dafe17192efcb840ff467b68fe4417d59
Bernardo Ascensao <b.ascensao@gmail.com>
2023-07-09 13:38:36 +0100

    project rename

---
+++ file2text_text_extractor/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace c_sharp_file2text
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.AllowDrop = true;
+            textBox1.Font = new Font("SF Pro Text", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new System.Drawing.Point(0, 0);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(1263, 681);
+            textBox1.TabIndex = 0;
+            textBox1.DragDrop += textBox1_DragDrop;
+            textBox1.DragEnter += textBox1_DragEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1264, 681);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new System.Drawing.Size(1280, 720);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "File2text - Text Extractor";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
